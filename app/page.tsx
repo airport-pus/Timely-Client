0ec3c209c5f53100377702c7a26240dc1df8f8ac
huise0ng <shinhuiseong07@gmail.com>
2025-02-28 17:28:05 +0900

    refctor :: 검색 바 컴포넌트화 시키고 임포트

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useRef, useLayoutEffect } from 'react';
 import Header from './header/header';
+import Search from './search/search';
 
 type EditableCellProps = {
   value: string;
@@ -214,43 +215,7 @@ export default function Home() {
       >
         <div className="grid grid-cols-1 gap-0 mx-[580px] items-center justify-items-center p-8 pb-20 text-black">
           <Header />
-          
-          <div className="flex space-x-4 mt-6 w-full">
-            <div className="flex items-center border border-gray-300 rounded-md w-full px-4 py-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-gray-500 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-4.35-4.35M9 17a8 8 0 100-16 8 8 0 000 16z"
-                />
-              </svg>
-              <input type="text" placeholder="학교명 검색" className="w-full outline-none" />
-            </div>
-            <select className="border border-gray-300 px-4 py-2 rounded-md">
-              <option value="">학년 선택</option>
-              <option value="1">1학년</option>
-              <option value="2">2학년</option>
-              <option value="3">3학년</option>
-              <option value="4">4학년</option>
-              <option value="5">5학년</option>
-              <option value="6">6학년</option>
-            </select>
-            <select className="border border-gray-300 px-4 py-2 rounded-md">
-              <option value="">반 선택</option>
-              {[...Array(12)].map((_, i) => (
-                <option key={i} value={i + 1}>
-                  {i + 1}반
-                </option>
-              ))}
-            </select>
-          </div>
+          <Search />
 
           <div className="w-full mt-8 overflow-x-auto">
             <div className="text-gray300 text-center text-[15px] mb-3">
